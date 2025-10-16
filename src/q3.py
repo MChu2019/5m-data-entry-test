ed5203063d55a6107d12a60ea382f760fd16310a
MChu2019 <57430384+MChu2019@users.noreply.github.com>
2025-10-16 09:21:08 +0800

    Update q3.py

--- src/q3.py
+++ src/q3.py
@@ -7,8 +7,17 @@ def update_dictionary(dct, key, value):
     """
     return
 
+    """
+     if key in dct:
+        print(f"Original value for '{key}': {dct[key]}")
+    dct[key] = value
+    return dct
+
 
 # Task 2
 # Invoke the function "update_dictionary" using the following scenarios:
 # - {}, "name", "Alice"
 # - {"age": 25}, "age", 26
+
+ print(update_dictionary({}, "name", "Alice"))         # Adding new key
+print(update_dictionary({"age": 25}, "age", 26))      # Updating existing key
